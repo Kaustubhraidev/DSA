aba7293cee21f2342962a277b904faa5005b450c
Kaustubh Dev Rai <89676799+Kaustubhraidev@users.noreply.github.com>
2024-09-14 18:20:06 +0530

    Add files via upload

---
+++ condecintobin.java
@@ -0,0 +1,23 @@
+package DSA;
+import java.util.*;
+public class condecintobin {
+    public static void main(String[] args) {
+      int n = 4;
+      int pos = 2;
+      int bitmask = 1<<pos;
+      if((bitmask & n) == 0){
+          System.out.println("bit was zero");
+      }else {
+          System.out.println("bit was one");
+      }
+
+      System.out.println("Set bit :-");
+      int m = 5;
+      int post = 1;
+      int bit = 1<<post;
+      int newnumber = bit | m;
+      System.out.println(newnumber);
+
+    }
+
+}
