f972d70ee8d6c0ddf589d8670456adfb4f208564
Kaustubh Dev Rai <kaustubhrai9125@gmail.com>
2025-07-10 18:48:54 +0530

    Create GraphValidTree.java

---
+++ 75_leetcode/GraphValidTree.java
@@ -0,0 +1,40 @@
+class solution {
+  public boolean validTree(int n, int[][] edges){
+
+    if(edges.length != n-1){
+      return false;
+    }
+
+    List<List<Integer>> adjacentList = new ArrayList();
+    for(int i=0; i<n; i++){
+      adjacencyList.add(new ArrayList<>());
+    }
+
+    for(int[] edge:edges){
+      adjacencyList.get(edge[0]).add(edge[1]);
+      adjacencyList.get(edge[1]).add(edge[0]);
+    }
+    Stack<Integer> stack = new Stack<>();
+    HashSet<Integer> visited = new HashSet<>();
+
+    stack.push(0);
+    visited.add(0);
+
+    while(!stack.isEmpty()){
+      int node = stack.pop();
+      for(int neighbour:adjacencyList.get(node)){
+        if(visited.contains(neighbour)){
+          continue;
+        }
+        visited.add(neighbour);
+        stack.push(neighbour)
+      }
+    }
+ if(visited.size() ==n){
+   return true;
+ }
+    return false;
+
+    
+    }
+}
